c80fc1529edcae2ea8867377008d9e4f80189819
Pedro Ledesma <ledesmacitolerpedrito@gmail.com>
2026-03-31 21:39:31 +0200

    Fix activabar middleware exports

---
+++ src/activabar/middleware/security-headers.js
@@ -0,0 +1,9 @@
+function securityHeaders(_req, res, next) {
+  res.setHeader("X-Content-Type-Options", "nosniff");
+  res.setHeader("X-Frame-Options", "DENY");
+  res.setHeader("Referrer-Policy", "same-origin");
+  res.setHeader("Permissions-Policy", "camera=(), microphone=(), geolocation=()");
+  next();
+}
+
+module.exports = { securityHeaders };
